58c565c523c76cc2d951acdd85a3cd7bbe77a122
trapzcurrency <anupgupta1113@gmail.com>
2023-12-16 13:59:44 +0530

    hec

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
     integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
     crossorigin="anonymous" referrerpolicy="no-referrer" />
-
+  <link rel="stylesheet" type="text/css" href="style.css">
 <!doctype html>
 <html lang="en">
 <head>
@@ -14,160 +14,89 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Trapzcurrency</title>
 </head>
-<body style="background-image: url('1.jpg')">
+<body>
   <div class="container-fluid">
-    <nav class="navbar navbar-expand-md bg-body-tertiary responsive"
-      style="border-radius: 100%; border: 2px solid black; background-color: #EA0000!important;">
-      <div class="container-fluid" style="background-color: #DED8D8; color: white; border-radius:10px 10px 10px 10px;border: 2px solid black;">
-        <div class="collapse navbar-collapse" id="navbarScroll">
-          <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" style="font-weight:bold; color:black;" href="#"><i
-                  class="fa-solid fa-house"></i> Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#" style="font-weight:bold;"><i class="fa-solid fa-diagram-project"></i>
-                Projects</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" style="font-weight:bold;" href="#" role="button"
-                data-bs-toggle="dropdown" aria-expanded="false">
-                Data Table
-              </a>
-              <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="myResume.html">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-                <li>
-                  <hr class="dropdown-divider">
-                </li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
-              </ul>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" style="font-weight:bold;" aria-disabled="true">UseFull Links</a>
-            </li>
-          </ul>
-          <div class="gx-4 mr-5">
-            <span style="font-weight:bold; font-size: 30px; color: red;">
-              <i class="fa-brands fa-instagram"></i>
-            </span>
-            <span style="font-weight:bold; font-size: 30px; color:blue;">
-              <i class="fa-brands fa-linkedin"></i>
-            </span>
-          </div>
-        </div>
-      </div>
-    </nav>
   </div>
+ 
   <div class="container-fluid mt-2">
-    <div class="card" style="background-color:#16F3DC ;">
+    <div class="row justify-content-center">
+        <a href="javascript:void(0)" class="btn btn-warning mr-2 headerButtons" style="color:white;" onclick="academics(1)">Academics</a>
+        <a href="javascript:void(0)" class="btn btn-primary headerButtons" onclick="academics(2)">Skills</a>
+        <a href="javascript:void(0)" class="btn headerButtons" style="background-color:#0077b5; color:white;"><i class="fa-brands fa-linkedin"></i> Linkedin</a>
+        <a href="javascript:void(0)" class="btn headerButtons" style="background-color: #F56040; color:white;"><i class="fa-brands fa-instagram"></i> Instagram</a>
+        <a href="myResume.html" class="btn headerButtons" style="background-color: black; color:white;"><i class="fa-solid fa-file"></i> Resume</a>
+    </div>
+    <div class="card mt-2">
       <div class="card-header" style="background-color:#130603;  color:white;">
         Trapz Welcomes You
       </div>
-      <div class="card-body mt-2"
-        style="background-color: #130603;height: 500px; color: white; opacity: 100%; border-radius: 1%; ">
+      <div class="card-body mt-2"style="background-image:url(naruto.png);height: 300px; color: white; opacity: 100%; border-radius: 1%; ">
         <div class="row">
-          <p id="pText"></p>
           <div class="col-md-8">
+            <p id="pText"></p>
             <p id="skilsTransition">Click to Check My Skills.</p>
           </div>
-          <div class=" col-md-4 justify-content-md-center">
+          <div class="col-md-4 d-flex justify-content-center" style="">
             <h4> Using snapchat For Disguise </h4>
             <img src="profile1.jpg" style="height: 200px; width: 200px; border-radius:100%; opacity: 80%;" alt="Avatar">
           </div>
         </div>
       </div>
     </div>
-    <div class="row">
-      <div class="col-md-3 mt-1">
-        <div class="card" style="opacity: 50%;">
-          <div class="card-header">Academic Details</div>
-          <div class="card-body" >
-            <p style="font-weight:bold;">xyz College</p>
-            <p>xyz College</p>
-            <p>xyz College</p>
-          </div>
+    <div class="content mt-2" style="background-color:white;">
+        <div class="row text-center">
+            <h4 style="background-color:red;">Hobies</h4>
         </div>
-      </div>
-       <div class="col-md-3 mt-1">
-        <div class="card">
-          <div class="card-header">Skills</div>
-          <div class="card-body">
-            <p>xyz College</p>
-            <p>xyz College</p>
-            <p>xyz College</p>
-          </div>
+        <div class="row">
+          <p>1) <span><i class="fa-solid fa-futbol"></i></span> Football</p>
+          <p>2) <span><i class="fa-solid fa-fire"></i></span> Night Camping</p>
+          <p>3)<span><i class="fa-solid fa-leaf"></i></span> Spending Time In nature</p>
         </div>
-      </div>
-       <div class="col-md-3 mt-1">
-        <div class="card">
-          <div class="card-header">Hobies</div>
-          <div class="card-body">
-            <p>1)<span><i class="fa-solid fa-futbol"></i></span> Football</p>
-            <p>2)<span><i class="fa-solid fa-fire"></i></span> Night Camping</p>
-            <p>3)<span><i class="fa-solid fa-leaf"></i></span> Spending Time In nature</p>
-          </div>
-        </div>
-      </div>
-       <div class="col-md-3 mt-1">
-        <div class="card">
-          <div class="card-header">Projects</div>
-          <div class="card-body"></div>
-        </div>
-      </div>
-       <div class="col-md-3 mt-1">
-        <div class="card">
-          <div class="card-header">Work Experience</div>
-          <div class="card-body"></div>
-        </div>
-      </div>
-       <div class="col-md-3 mt-1">
-        <div class="card">
-          <div class="card-header">Favourite Sports</div>
-          <div class="card-body"></div>
-        </div>
-      </div>
-       <div class="col-md-3 mt-1">
-        <div class="card">
-          <div class="card-header">Check</div>
-          <div class="card-body"></div>
-        </div>
-      </div>
-       <div class="col-md-3 mt-1">
-        <div class="card">
-          <div class="card-header">Check</div>
-          <div class="card-body"></div>
-        </div>
-      </div>
-
-
     </div>
+    
   </div>
 </body>
 <div class="card mt-2">
-  <div class="card-header">
+  <div class="card-header d-flex justify-content-center">
     Copy right Discalimer = Naruto
   </div>
-  <div class="card-body">
+  <div class="card-body text-center">
     <h5 class="card-title">I Can coock PHP</h5>
     <p class="card-text">Coding is quite easy in gaming laptop.</p>
     <a href="#" class="btn btn-primary">Anime</a>
   </div>
 </div>
 </html>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-  integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/js/all.min.js"
-  integrity="sha512-uKQ39gEGiyUJl4AI6L+ekBdGKpGw4xJ55+xyJG7YFlJokPNYegn9KwQ3P8A7aFQAUtUsAQHep+d/lrGqrbPIDQ=="
-  crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/js/fontawesome.min.js"
-  integrity="sha512-64O4TSvYybbO2u06YzKDmZfLj/Tcr9+oorWhxzE3yDnmBRf7wvDgQweCzUf5pm2xYTgHMMyk5tW8kWU92JENng=="
-  crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"
   integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g=="
   crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/5.5.2/bootbox.min.js"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/5.5.2/bootbox.min.js" integrity="sha512-RdSPYh1WA6BF0RhpisYJVYkOyTzK4HwofJ3Q7ivt/jkpW6Vc8AurL1R+4AUcvn9IwEKAPm/fk7qFZW3OuiUDeg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/5.5.2/bootbox.js" integrity="sha512-K3MtzSFJk6kgiFxCXXQKH6BbyBrTkTDf7E6kFh3xBZ2QNMtb6cU/RstENgQkdSLkAZeH/zAtzkxJOTTd8BqpHQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+  integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/js/all.min.js"
+  integrity="sha512-uKQ39gEGiyUJl4AI6L+ekBdGKpGw4xJ55+xyJG7YFlJokPNYegn9KwQ3P8A7aFQAUtUsAQHep+d/lrGqrbPIDQ=="
+  crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/js/fontawesome.min.js"
+  integrity="sha512-64O4TSvYybbO2u06YzKDmZfLj/Tcr9+oorWhxzE3yDnmBRf7wvDgQweCzUf5pm2xYTgHMMyk5tW8kWU92JENng=="
+  crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.js"
   integrity="sha512-+k1pnlgt4F1H8L7t3z95o3/KO+o78INEcXTbnoJQ/F2VqDVhWoaiVml/OEHv9HsVgxUaVW+IbiZPUJQfF/YxZw=="
   crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
 <script src="https://unpkg.com/typewriter-effect@latest/dist/core.js"></script>
 <script src="script.js"></script>
+<script src="bootbox.all.js"></script>
+<script src="bootbox.all.min.js"></script>
--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
    
-
-        new Typewriter('#pText', {
+    new Typewriter('#pText', {
   		strings: ['Hello There','This is trapzcurrency.'],
 		autoStart: true,
+		loop:true,
 		});
 
        $('#skilsTransition').click(function(){
@@ -16,9 +16,56 @@ $(document).ready(function() {
 function typing(){
 	
 	new Typewriter('#skilsTransition', {
-  		strings: ['	"Highly skilled web developer with expertise in a powerful toolkit of technologies: PHP for server-side scripting, JavaScript for dynamic interactivity, HTML5 for structuring content, CSS for elegant design, and MySQL for efficient data management. Proficient in creating responsive and interactive web applications that blend functionality and aesthetics, delivering seamless user experiences."'],
+		strings: ['	"Highly skilled web developer with expertise in a powerful toolkit of technologies: PHP for server-side scripting, JavaScript for dynamic interactivity, HTML5 for structuring content, CSS for elegant design, and MySQL for efficient data management. Proficient in creating responsive and interactive web applications that blend functionality and aesthetics, delivering seamless user experiences."'],
 		autoStart: true,
 		delay: 75,
+		loop:true,
 		});
+}
+
+function academics(type) {
+
+	var html = '';
+	var title='';
+
+	if(type==1){
+
+		title = 'Academics';
+
+		 html='<p>1) Bharatiya Jain Sanghatana College.</p>'+
+	            '<p>2) S.m.joshi jr College.</p>'+
+	            '<p>3) St.Oscar convent high school.</p>';	
+
+	}
+	if(type=='2'){
+
+		title='Skills';
+
+		html='<p>1) Data Processing and Logic Building.</p>'+
+	            '<p>2) Specifif skills : API"s,Ajax,Jquery,JSON,ARRAY,Objects.</p>'+
+	            '<p>3) Proficiency in Laravel,Code-Igniter and MVC concepts.</p>'+
+	            '<p>4) Database : Mysql,Yog sql.</p>';
+	}
+	
+	var box= bootbox.alert({
+		title:title,
+		size:'large',
+		message:html,
+		closeButton:false
+	})
+
+	if(type==1){
+
+		box.find('.modal-header').css({'background-color': '#D3D3D3' ,'color':'#36454F'})
+		box.find('.modal-body').css({'background-color': '#36454F' ,'color':'#D3D3D3'})
+		box.find('.modal-footer').css({'background-color': '#36454F' ,'color':'#D3D3D3'})
+
+	}else{
+
+		box.find('.modal-header').css({'background-color': '#0096FF' ,'color':'#ffd700'})
+		box.find('.modal-body').css({'background-color': '#36454F' ,'color':'#D3D3D3'})
+		box.find('.modal-footer').css({'background-color': '#36454F' ,'color':'#D3D3D3'})
+	}
+	
 }
 
