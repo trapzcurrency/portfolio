9f7e39cbbd989651bb97e38875b26c1ffbfbded6
trapzcurrency <ganup909@gmail.con>
2024-02-25 22:49:39 +0530

    composer

--- index.php
+++ index.php
@@ -31,8 +31,7 @@
         if ($conn->connect_error) {
           die("Connection failed: " . $conn->connect_error);
         }else{
-              echo "Connected successfully";
-        print_r("successfully");
+             echo "hello";
         }
           
     ?>
